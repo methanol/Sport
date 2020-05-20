6fa3d9d0a599268ab41aa6b5d438908f12a5180c
methanol <methanol.b@gmail.com>
2020-05-20 20:51:02 +0700

    Revert "add basic admin functions"
    This reverts commit 29395b6aa7e45836e9b6c01a059344608aa2438d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { StoreFirstGuard } from "./storeFirst.guard";
       { path: "store", component: StoreComponent, canActivate: [StoreFirstGuard] },
       { path: "cart", component: CartDetailComponent, canActivate: [StoreFirstGuard] },
       { path: "checkout", component: CheckoutComponent, canActivate: [StoreFirstGuard] },
-      { path: "admin", loadChildren: "./admin/admin.module#AdminModule", canActivate: [StoreFirstGuard]},
       { path: "**", redirectTo: "/store" } // TODO move navigation to AppRoutingModule
 ])
   ],
